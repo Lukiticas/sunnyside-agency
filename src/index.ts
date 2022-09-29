cfdff07ef8fe0f25609e15b2063a60e15a985ec5
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-09-29 00:31:14 -0300

    working on animations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-const Images = document.querySelectorAll(".box-container");
+const $ = (name: string) => document.querySelectorAll(name);
+const id = (name: string) => document.getElementById(name);
+
+const toggleClass = (el: Element, className: string, forClass?: string) => {
+  if (forClass === undefined) {
+    el.classList.toggle(className);
+  } else {
+    if (el.classList.contains(className)) {
+      el.classList.remove(className);
+      el.classList.add(forClass);
+    } else {
+      el.classList.add(className);
+      el.classList.remove(forClass);
+    }
+  }
+};
+
+const arrowDown = $(".hero-section__text--arrow-down")[0] as Element;
+const LogoBTT = $(".logo");
+const hamburguer = $(".hamburguer");
+const popUp = $(".pop-up");
+
+arrowDown.addEventListener("click", () => {
+  id("index")!.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+LogoBTT.forEach((el) =>
+  el.addEventListener("click", () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  })
+);
+
+hamburguer.forEach((el) => {
+  el.addEventListener("click", () => {
+    toggleClass(el, "ph-list-fill", "ph-x-fill");
+    toggleClass(popUp[0], "off");
+  });
+});
