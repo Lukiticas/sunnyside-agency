d59a0133a775eac863389a0add1095045f45b557
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-09-29 15:30:29 -0300

    App done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 const $ = (name: string) => document.querySelectorAll(name);
 const id = (name: string) => document.getElementById(name);
 
+/* ELEMENTS */
+
+const arrowDown = $(".hero-section__text--arrow-down")[0] as Element;
+const LogoBTT = $(".logo");
+const hamburguer = $(".hamburguer");
+const popUp = $(".pop-up");
+const sectionWrappers = $(".info-section");
+const sections = $(".info-section > *");
+
+/* TOGGLE CLASS FOR SCROLLING LOADING */
+
 const toggleClass = (el: Element, className: string, forClass?: string) => {
   if (forClass === undefined) {
     el.classList.toggle(className);
@@ -15,22 +26,54 @@ const toggleClass = (el: Element, className: string, forClass?: string) => {
   }
 };
 
-const arrowDown = $(".hero-section__text--arrow-down")[0] as Element;
-const LogoBTT = $(".logo");
-const hamburguer = $(".hamburguer");
-const popUp = $(".pop-up");
+(() => {
+  sections.forEach((section) => toggleClass(section, "opacity-zero"));
+})();
+
+/* OBSERVER FOR SCROLLING LOADING */
+
+const options = {
+  threshold: 0.1,
+};
+
+const callback: IntersectionObserverCallback = (entries, observer) => {
+  entries.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.remove("opacity-zero");
+      observer.unobserve(item.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+/* EVENT LISTENERS */
+
+sections.forEach((wrapper) => {
+  observer.observe(wrapper);
+});
 
 arrowDown.addEventListener("click", () => {
+  /* SCROLL TO MID BOTTOM */
+  id("index")!.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+arrowDown.addEventListener("keydown", (evt) => {
+  /* SCROLL TO MID BOTTOM */
+  const button = <KeyboardEvent>evt;
+  if (button.key !== "Enter") return;
   id("index")!.scrollIntoView({ behavior: "smooth", block: "center" });
 });
 
 LogoBTT.forEach((el) =>
+  /* SCROLL TO TOP BOTTOM */
   el.addEventListener("click", () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   })
 );
 
 hamburguer.forEach((el) => {
+  /* HAMBUERGUER MENU BUTTOM */
   el.addEventListener("click", () => {
     toggleClass(el, "ph-list-fill", "ph-x-fill");
     toggleClass(popUp[0], "off");
